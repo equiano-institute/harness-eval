cd2d297bbcfe0b608eb100d6b74d49aa2d12bac1
Israel Abebe Azime <se.israel.abebe@gmail.com>
2024-07-20 02:06:50 +0200

    complted truthfulqa tasks

---
+++ run_uhura_gen.sh
@@ -0,0 +1,23 @@
+models=(
+  "meta-llama/Meta-Llama-3-8B"
+)
+
+languages=('am' 'ha' 'sw' 'yo')
+
+for model in "${models[@]}"
+do
+        for lan in "${languages[@]}"; do
+
+            echo "$lan"_truthfulqa_gen
+            python3 -m lm_eval --model hf \
+                    --model_args pretrained=$model,parallelize=True  \
+                    --tasks "$lan"_truthfulqa_gen   \
+                    --batch_size 1 \
+                    --verbosity DEBUG \
+                    --output_path "output/$model" \
+                    --log_samples \
+                    --limit 5 \
+                    --wandb_args project=uhura,name="$lan"_generation
+        done
+                     
+done
