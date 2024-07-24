e666392d1524b8c18c92f4b4b1c0708012a29e41
Israel Abebe Azime <se.israel.abebe@gmail.com>
2024-07-24 15:06:42 +0200

    upate

--- run_uhura_mc1.sh
+++ run_uhura_mc1.sh
@@ -1,5 +1,5 @@
 models=(
-  "meta-llama/Meta-Llama-3-8B"
+  "LLaMAX/LLaMAX3-8B"
 )
 
 languages=('am' 'ha' 'sw' 'yo')
@@ -7,17 +7,19 @@ languages=('am' 'ha' 'sw' 'yo')
 for model in "${models[@]}"
 do
         for lan in "${languages[@]}"; do
-
-            echo "$lan"_truthfulqa_gen
-            python3 -m lm_eval --model hf \
-                    --model_args pretrained=$model,parallelize=True  \
-                    --tasks "$lan"_truthfulqa_mc1   \
-                    --batch_size 1 \
-                    --verbosity DEBUG \
-                    --output_path "output/$model" \
-                    --log_samples \
-                    --limit 5 \
-                    --wandb_args project=uhura,name="$lan"_truthfulqa_mc1
+                for examples in 0 5
+                do
+                        echo "$lan"_truthfulqa_gen_$examples
+                        python3 -m lm_eval --model hf \
+                                --model_args pretrained=$model,parallelize=True  \
+                                --tasks "$lan"_truthfulqa_mc1   \
+                                --batch_size 1 \
+                                --num_fewshot $examples \
+                                --verbosity DEBUG \
+                                --output_path "output/$model" \
+                                --log_samples \
+                                --wandb_args project=uhura,name="$lan"_truthfulqa_mc1_$examples
+                done
         done
                      
 done
