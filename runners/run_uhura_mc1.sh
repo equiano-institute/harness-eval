e357054b42674bba09aab8b330ce29b8a7cd1b13
Israel Abebe Azime <se.israel.abebe@gmail.com>
2024-08-24 08:13:33 +0200

    runners/run_uhura_mc1.sh

--- runners/run_uhura_mc1.sh
+++ runners/run_uhura_mc1.sh
@@ -1,8 +1,9 @@
 models=(
-  ""meta-llama/Meta-Llama-3-8B""
+  "meta-llama/Meta-Llama-3-8B-Instruct"
+  "meta-llama/Meta-Llama-3-8B"
 )
 
-languages=('am' 'en' 'ha' 'nso' 'sw' 'yo')
+languages=('en' 'am'  'ha' 'nso' 'sw' 'yo')
 prompt_tempaltes=('t1' 't2' 't3' 't4' 't5')
 
 for model in "${models[@]}"
@@ -10,7 +11,7 @@ do
         for lan in "${languages[@]}"; do
                 for examples in 0 5; do
                         for temp in "${prompt_tempaltes[@]}"; do
-                                echo "$lan"_truthfulqa_gen_$examples
+                                echo "$lan"_truthfulqa_mc1_$temp
                                 python3 -m lm_eval --model hf \
                                         --model_args pretrained=$model,parallelize=True  \
                                         --tasks "$lan"_truthfulqa_mc1_$temp   \
@@ -18,7 +19,6 @@ do
                                         --num_fewshot $examples \
                                         --verbosity DEBUG \
                                         --output_path "output/$model" \
-                                        --limit 5 \
                                         --log_samples \
                                         --wandb_args project=uhura-cluster,name="$lan"_truthfulqa_mc1_"$examples"_"$model"_"$temp"
                         done
