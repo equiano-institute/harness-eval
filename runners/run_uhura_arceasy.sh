af0c5abf95ea88213805ed295600eac275fb34c6
Israel Abebe Azime <se.israel.abebe@gmail.com>
2024-09-24 16:47:38 +0200

    update

--- runners/run_uhura_arceasy.sh
+++ runners/run_uhura_arceasy.sh
@@ -6,25 +6,23 @@ models=(
   "LLaMAX/LLaMAX3-8B"
 )
 
-languages=('am' 'ha' 'nso' 'sw' 'yo')
+
 prompt_tempaltes=('t1' 't2' 't3' 't4' 't5')
 
 for model in "${models[@]}"
 do
-        for lan in "${languages[@]}"; do
-               for examples in 0 5 ; do
-                        for temp in "${prompt_tempaltes[@]}"; do
-                                echo "$lan"_arc_challenge
-                                python3 -m lm_eval --model hf \
-                                        --model_args pretrained=$model,parallelize=True  \
-                                        --tasks "$lan"_arc_challenge   \
-                                        --batch_size 1 \
-                                        --verbosity DEBUG \
-                                        --num_fewshot $examples \
-                                        --output_path "output/$model" \
-                                        --log_samples \
-                                        --wandb_args project=uhura,name="$lan"_arc_challenge_"$model"_"$temp"
-                        done
+        for examples in 0 5 ; do
+                for temp in "${prompt_tempaltes[@]}"; do
+                        echo "$lan"_arc_challenge
+                        python3 -m lm_eval --model hf \
+                                --model_args pretrained=$model,parallelize=True  \
+                                --tasks am_arc_challenge_$temp,am_arc_challenge_$temp,en_arc_challenge_$temp,ha_arc_challenge_$temp,nso_arc_challenge_$temp,sw_arc_challenge_$temp,yo_arc_challenge_$temp   \
+                                --batch_size 1 \
+                                --verbosity DEBUG \
+                                --num_fewshot $examples \
+                                --output_path "output/$model" \
+                                --log_samples \
+                                --wandb_args project=uhura,name=arc_challenge_"$model"_"$temp"
                 done
         done
                      
